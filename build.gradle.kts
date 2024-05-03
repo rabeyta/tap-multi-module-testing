4477ce2b1c7d909a608ea4ddb52080369a59b0b9
Robert Abeyta <robert.abeyta@broadcom.com>
2024-05-03 09:56:04 -0700

    add module detection

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.dropbox.affectedmoduledetector") version "0.3.1"
+}
+
+affectedModuleDetector {
+    baseDir = "${project.rootDir}"
+    pathsAffectingAllModules = setOf("buildSrc")
+    logFilename = "output.log"
+    logFolder = "${project.rootDir}/output"
+    compareFrom = "PreviousCommit" //default is PreviousCommit
+    ignoredFiles = setOf(".*\\.md", ".*\\.txt", ".*README")
+    includeUncommitted = true
+}
+
+tasks.register("printProjectName") {
+    doLast {
+        println(project.name)
+    }
+}
+
+// Register Custom Task with root project
+tasks.register<AffectedTask>("affected") {
+    group = "Affected Module Detector"
+    description = "print all affected subprojects due to code changes"
+}
+
+// Define Custom Task
+open class AffectedTask : DefaultTask() {
+    @TaskAction
+    fun printAffected() {
+        project.subprojects.forEach {
+            println("Is ${it.name} Affected? : " + com.dropbox.affectedmoduledetector.AffectedModuleDetector.isProjectAffected(it))
+        }
+    }
+}
