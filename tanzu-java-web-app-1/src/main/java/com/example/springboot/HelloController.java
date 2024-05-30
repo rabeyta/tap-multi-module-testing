ec8b2bbd061adf87a429cbce359d3982c3486b11
Robert Abeyta <robert.abeyta@broadcom.com>
2024-05-30 14:19:53 -0700

    another

--- tanzu-java-web-app-1/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app-1/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	private static final String someConst = "3rd";
+	private static final String someConst = "4th";
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot + Tanzu! - app 1" + " some const of " + someConst;
