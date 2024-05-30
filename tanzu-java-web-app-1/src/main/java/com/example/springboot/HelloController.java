8aad7b5c839567d5647a87d3cbe47b6daa26880d
Robert Abeyta <robert.abeyta@broadcom.com>
2024-05-30 14:11:14 -0700

    another update to 1

--- tanzu-java-web-app-1/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app-1/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	private static final String someConst = "updated";
+	private static final String someConst = "3rd";
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot + Tanzu! - app 1" + " some const of " + someConst;
