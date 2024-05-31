3be145e04ed4f06560e7402909a02218ba4efbb3
Robert Abeyta <robert.abeyta@broadcom.com>
2024-05-31 13:06:51 -0700

    2

--- tanzu-java-web-app-2/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app-2/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        String someValue = "please";
+        String someValue = "tset";
         return "Greetings from Spring Boot + Tanzu! - app 2\n\n" + "value from variable: " + someValue;
     }
 
